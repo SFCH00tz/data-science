a9c35ab9611274cb737a44924ef6474e7eeec1ff
Dakota Dunn <dakota@Dakotas-MacBook-Pro.local>
2020-10-19 21:32:06 -0700

    Update to lines 28 and 29 spacing

--- CrashData.py
+++ CrashData.py
@@ -25,8 +25,8 @@ def get_prev_game(hash_code):
     m = hashlib.sha256()
     m.update(hash_code.encode("utf-8"))
     return m.hexdigest()
-  
-  game_hash = '100af1b49f5e9f87efc81f838bf9b1f5e38293e5b4cf6d0b366c004e0a8d9987' # Update to latest game's hash for more results
+
+game_hash = '100af1b49f5e9f87efc81f838bf9b1f5e38293e5b4cf6d0b366c004e0a8d9987' # Update to latest game's hash for more results
 first_game = "77b271fe12fca03c618f63dfb79d4105726ba9d4a25bb3f1964e435ccf9cb209"
 
 results = []
@@ -35,7 +35,7 @@ while game_hash != first_game:
     count += 1
     results.append(get_result(game_hash))
     game_hash = get_prev_game(game_hash)
-    
+
 results = np.array(results)
 
 multiplier = 1.99
