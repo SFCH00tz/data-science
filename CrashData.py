55000b01c5054b31c501b8c6b88f07ca6f0d79f0
SFCH00tz <73153176+SFCH00tz@users.noreply.github.com>
2020-10-19 21:30:40 -0700

    Update CrashData.py
    Update from MindingTheData source code

--- CrashData.py
+++ CrashData.py
@@ -1,1 +1,87 @@
 # Crash Game Data Sience
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
+import random
+import string
+import hmac
+
+e = 2**52
+salt = "0000000000000000000fa3b65e43e4240d71762a5bf397d5304b2596d116859c"
+
+game_hash = '100af1b49f5e9f87efc81f838bf9b1f5e38293e5b4cf6d0b366c004e0a8d9987'
+
+def get_result(game_hash):
+    hm = hmac.new(str.encode(game_hash), b'', hashlib.sha256)
+    hm.update(salt.encode("utf-8"))
+    h = hm.hexdigest()
+    if (int(h, 16) % 33 == 0):
+        return 1
+    h = int(h[:13], 16)
+    e = 2**52
+    return (((100 * e - h) / (e-h)) // 1) / 100.0
+
+def get_prev_game(hash_code):
+    m = hashlib.sha256()
+    m.update(hash_code.encode("utf-8"))
+    return m.hexdigest()
+  
+  game_hash = '100af1b49f5e9f87efc81f838bf9b1f5e38293e5b4cf6d0b366c004e0a8d9987' # Update to latest game's hash for more results
+first_game = "77b271fe12fca03c618f63dfb79d4105726ba9d4a25bb3f1964e435ccf9cb209"
+
+results = []
+count = 0
+while game_hash != first_game:
+    count += 1
+    results.append(get_result(game_hash))
+    game_hash = get_prev_game(game_hash)
+    
+results = np.array(results)
+
+multiplier = 1.99
+(results <= multiplier).mean(), 1/33 + (32/33)*(.01 + .99*(1 - 1/multiplier))
+
+multiplier = 1.05
+((1/33) + (32/33)*(.01 + .99*(1 - 1/(multiplier-.01))))*-1 + (multiplier-1)*(1 - ((1/33) + (32/33)*(.01 + .99*(1 - 1/(multiplier-.01)))))
+
+(results < multiplier).mean() * -1 + (multiplier - 1)*(results >= multiplier).mean()
+
+import seaborn as sns
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+
+plt.hist(results, range=(0, 25))
+plt.title("Histogram of Game Results", fontsize=20)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.ylabel("Number of Games", fontsize=15)
+plt.xlabel("Multiplier", fontsize=15)
+
+def calculate_ev(multiplier):
+    return ((1/33) + (32/33)*(.01 + .99*(1 - 1/(multiplier-.01))))*-1 + (multiplier-1)*(1 - ((1/33) + (32/33)*(.01 + .99*(1 - 1/(multiplier-.01)))))
+
+xs = np.linspace(101, 1001, 901) / 100
+ys = [calculate_ev(x) for x in xs]
+y2s = [(results < x).mean() * -1 + (x - 1)*(results >= x).mean() for x in xs]
+
+plt.plot(xs, ys, linewidth=5)
+plt.xlabel("Multiplier", fontsize=15)
+plt.ylabel("Expected Value", fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.ylim(-.045, -.025)
+plt.plot(xs, y2s, linewidth=3)
+plt.title("Expected Value by Multiplier", fontsize=20)
+plt.legend(["Theoretical Results", "Actual Results"])
+
+negatives = []
+in_a_row = 0
+for multiplier in results:
+    if multiplier < 2:
+        in_a_row += 1
+    else:
+        in_a_row = 0
+    negatives.append(in_a_row)
+negatives = np.array(negatives)
+
+for i in range(1, 14):
+    print("Probability of Losing %d Game(s) in a Row:"%i, (negatives >= i).mean())
